610b2fee8958604db719222ce32d89131d9a6f5c
Aeol Kong <kongsdev@gmail.com>
2013-10-01 11:11:28 +0800

    bug fix

--- management/__init__.py
+++ management/__init__.py
@@ -1,10 +1,12 @@
+from os.path import basename, dirname
+
 from south.signals import post_migrate
 
 from corpdb.utils.initdb import init_db
 
 
 def migration_callback(**kwargs):
-    if kwargs['app'] == 'ypl':
+    if kwargs['app'] == basename(dirname(dirname(__file__))):
         init_db()
 
 
